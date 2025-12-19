f34088846545e02e73fb1f3e318da5571fa4500b
ncrujido <ncrujido01@auksilyo.com>
2025-12-19 13:21:48 +0800

    installing livewire and sqlite

---
+++ app/Livewire/Demos/ProjectDemo.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Demos;
+
+use Livewire\Component;
+
+class ProjectDemo extends Component
+{
+    public function render()
+    {
+        return view('livewire.demos.project-demo');
+    }
+}
---
+++ app/Livewire/Pages/ProjectShow.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Pages;
+
+use Livewire\Component;
+
+class ProjectShow extends Component
+{
+    public function render()
+    {
+        return view('livewire.pages.project-show');
+    }
+}
